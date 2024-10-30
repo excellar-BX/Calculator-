bfc2baffcf2e9304f4ac4e86f368cf9d14050de8
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2024-10-29 17:43:52 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+let screen = document.getElementById("screen")
+
+
+if(screen.value === '') {
+		screen.value = '0';
+	}
+
+
+ function appendToScreen(input){
+     if(screen.value === '0' || screen.value === '00' ){
+		screen.value = '';
+	}
+	if(screen.value === "SyntaxError" || screen.value === "ReferenceError" || screen.value === "NaN"){
+	    screen.value = '';
+	}
+    screen.value += input;
+}
+
+
+
+
+
+
+
+function clearScreen(){
+    screen.value = '0';
+}
+
+
+function backspace(){
+   if(screen.value === "SyntaxError" || screen.value === "ReferenceError" || screen.value === "NaN"){
+	    screen.value = "0";
+        
+    }else{
+    screen.value = screen.value.slice(0, - 1); 
+if(screen.value === '') {
+		screen.value = '0';
+   }
+    }
+}
+
+
+function calculate(){
+    try {
+    screen.value = eval(screen.value)
+    } catch (e) {
+        screen.value = e.name
+    }
+    
+}
+
+
